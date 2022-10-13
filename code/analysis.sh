29f89ef63f132c48950d08f0b4a29713aeea1fb8
Nicolas <nicolas_dintzner@hotmail.com>
2022-10-13 17:17:43 +0200

    cleaned up script file, added skeleton code for exercice

---
+++ code/analysis.sh
@@ -0,0 +1,28 @@
+# Analysis.sh 
+# This script is a wrapper around the readings-09.py module.
+#
+# The script takes as an input a file name, will run all 3 analysis possible
+# in the readings scripts on the input file (min, mean and max).
+#
+#
+#
+# The script takes the following input:
+# [1] output_folder: name of the folder in which to write the results
+# [2] file_name: name of the file to analyse
+# output:
+# 	located in "file_name.output.txt" (created by this script)
+#	contains in a single column the results from readings, all one after the other.
+
+
+#get the input file name from input parameters
+
+
+#create the output file (make sure it's empty, or empty it!)
+
+# combine input file name with the output folder to get the place where to write the results.
+
+#for each type of analysis, run the readings-09.py program
+# redirect the output to the output file.
+
+
+#Done!
